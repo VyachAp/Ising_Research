715bfeb0115b15760af71355bacbb17d99ffc5eb
Apasov Vyacheslav <vapasov@skytracking.ru>
2019-11-13 12:15:20 +0300

    Debug and refactoring

--- IsingModel1D.py
+++ IsingModel1D.py
@@ -2,22 +2,23 @@ import matplotlib.pyplot as plt
 from scipy import interpolate
 import numpy as np
 import math
-from random import randrange, random, choice
+import random
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
+BOLTSMAN_CONST = 1
+
 
 class Lattice1D:
     def __init__(self):
-        self.spins = 100
-        self.aveSpinGroup = 10
-        self.ising = []
-        self.alignmentE = -0.20
+        self.spins_amount = 100
+        self.iterations = 10 * self.spins_amount
+        self.spins = []
+        self.interaction_energy = 1  # J
         self.mu = 0.50
         self.B = -10.0
-        self.kB = 1.0
-        self.temperature = 2.5
+        self.temperature = 1
         self.time = 10000.
         self.timeplots = self.time / 100.0
         self.timeplotsteps = int(self.time / self.timeplots)
@@ -31,99 +32,94 @@ class Lattice1D:
         return 0
 
     def clear_configuration(self):
-        del self.ising[:]
+        del self.spins[:]
 
-    def fill_ising_configuration(self):
-        for each in range(self.spins):  # spin up is 1, spin down is -1
-            self.ising.append(-1)
+    def initiate_start_configuration(self):
+        for each in range(self.spins_amount):
+            g = random.uniform(0, 1)
+            if g >= 0.5:
+                self.spins.append(-1)
+            else:
+                self.spins.append(1)
+        print(len(self.spins))
 
     def calculate_energy(self):
         firstTerm = 0.0
         secondTerm = 0.0
-        for i in range(len(self.ising) - 1):
-            firstTerm += self.ising[i] * self.ising[i + 1]
-        logging.info(f'First term is {firstTerm}')
-        firstTerm *= self.alignmentE
-        for i in range(len(self.ising)):
-            secondTerm += self.ising[i]
-        logging.info(f'Second term is {secondTerm}')
-        firstTerm *= -self.B * self.mu
+        for i in range(len(self.spins) - 1):
+            firstTerm += self.spins[i] * self.spins[i + 1]
+        # logging.info(f'First term is {firstTerm}')
+        firstTerm *= -self.interaction_energy
+        for i in range(len(self.spins)):
+            secondTerm += self.spins[i]
+        # logging.info(f'Second term is {secondTerm}')
+        secondTerm *= -self.B * self.mu
         return firstTerm + secondTerm
 
-    def flip_spins(self, n, T, isingI):
-        flipped = []
-        isingO = isingI[:]
-        energy = self.calculate_energy()  # original energy
-        for i in range(n):
-            flipped.append(int(random.uniform(0, 1) * self.spins))  # choose a array of terms
-        for i in range(len(flipped)):  # flip those random terms in the output array
-            isingO[flipped[i]] = -1 * isingO[flipped[i]]
-        Etrial = self.calculate_energy()  # new energy
-        dE = float(Etrial - energy)  # you keep it if the new energy is lower than the old
-        # print "de is equal to %f" %dE
-        # print "dE is equal to %f" %dE
-        if dE > 0.0:  # if it is higher energy, then you don't automatically take it
-            logging.info(math.exp(-1. * dE / (self.kB * T)))
-            p = self.probability(math.exp(-1. * dE / (self.kB * T)))
-            if p > 0:  # with probablity (exp(-dE/ kbT)), keep the new arrangement
-                logging.info('Keeping arrangement')
-            else:  # otherwise, flip everything back
-                for i in range(len(flipped)):
-                    isingO[flipped[i]] *= -1
-                # print "keep the old arrangement00000"
-        count = 0
-        for i in range(len(isingI)):
-            if isingO[i] * isingI[i] == -1.:
-                count = count + 1
-        return isingO
+    def sweep_spin(self):
+        n = random.randint(0, self.spins_amount - 1)
+        self.spins[n] *= -1
+        return n  # return ordered number of reverted spin
 
-    def average_spin(self, array, i, spingroupsize):
-        summed = 0.0
-        for x in range(i, spingroupsize, 1):
-            summed += array[x]
-        logging.info(f'Average spins is {summed}')
-        return summed
+    def metropolis_algorithm(self):
+        energies = []
+        counter = 0
+        for i in range(self.iterations):
+            current_energy = self.calculate_energy()
+            k = self.sweep_spin()
+            probe_energy = self.calculate_energy()
+            if probe_energy <= current_energy:
+                logging.info("State accepted")
+                energies.append(probe_energy)
+                counter += 1
+            else:
+                p = self.probability(
+                    math.exp(-1. * (probe_energy - current_energy) / (BOLTSMAN_CONST * self.temperature)))
+                if p:
+                    logging.info("State accepted")
+                    energies.append(probe_energy)
+                    counter += 1
+                else:
+                    self.spins[k] *= -1
+                    logging.info("State reverted")
+                    energies.append(current_energy)
 
-    def plot_ising_model(self):
-        timeIsing = []
+        energy = self.calculate_average_energy(energies)
+        heat_capacity = self.calculate_heat_capacity(energies)
+        magnetization = np.sum(self.spins) / self.iterations
+
+        logging.info(f'Result of Metropolis algorithm: \n'
+                     f'State changed {counter} times '
+                     f'Energy = {energy} '
+                     f'Heat capacity = {heat_capacity} '
+                     f'Magnetization = {magnetization}')
+        self.plot_energy(energies)
+
+    def calculate_average_energy(self, energy):
+        return np.sum(energy) / self.iterations
+
+    def calculate_heat_capacity(self, energy):
+        squared_energy = np.power(energy, 2)
+        average_squared = self.calculate_average_energy(squared_energy)
+        average_in_square = math.pow(self.calculate_average_energy(energy), 2)
+        capacity = (average_squared - average_in_square) / (
+                self.iterations * BOLTSMAN_CONST * math.pow(self.temperature, 2))
+        return capacity
+
+    @staticmethod
+    def plot_energy(energy):
+        plt.plot(np.arange(0, len(energy), 1), energy, 'b--')
+        plt.title('Energy depending on iterations')
+        plt.xlabel('Iterations')
+        plt.ylabel('Energy')
+        plt.show()
+
+    def run(self):
         self.clear_configuration()
-        self.fill_ising_configuration()
-        for i in range(int(self.time)):
-            ising = self.flip_spins(1, self.temperature)
-            timeIsing.append(ising)
+        self.initiate_start_configuration()
+        self.metropolis_algorithm()
 
-        print
-        "len ising %d" % len(timeIsing)
-        print
-        "len(timeIsing[1]) %d" % len(timeIsing[1])
 
-        fig1 = plt.figure(1)
-        scatter = []
-        scatterplot = []
-
-        # scatter1 = []
-        # this is to make the plots
-
-        for t in range(int(self.time)):
-            #  del scatter1[:]
-            for i in range(len(self.ising)):
-                if timeIsing[t][i] == -1:
-                    # scatter1.append((t, i))
-                    scatter.append((t, i))
-                    if t % self.timeplotsteps == 0:
-                        scatterplot.append((t, i))
-        #  scatter.append(scatter1)
-        # for i in range(len(scatter)):
-        #  # iterate through columns
-        # for j in range(len(scatter[0])):
-        #    scatterA.append(scatter[i][j])
-
-        diff = []
-        for i in range(len(timeIsing[0])):
-            diff.append(timeIsing[0][i] - timeIsing[len(timeIsing) - 1][i])
-        # sum(diff)
-        # print diff
-
-        plt.scatter([pt[0] for pt in scatterplot], [pt[1] for pt in scatterplot])
-        # plt.scatter(*zip(*scatter))
-        fig1.show()
+if __name__ == '__main__':
+    lattice = Lattice1D()
+    lattice.run()
