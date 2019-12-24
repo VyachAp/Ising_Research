b814b5bfafee9d90a85520036f8a0cd963b3862e
Apasov Vyacheslav <vapasov@skytracking.ru>
2019-12-24 15:52:11 +0300

    wolff algorithm released

--- IsingModel2D.py
+++ IsingModel2D.py
@@ -14,7 +14,7 @@ BOLTSMAN_CONST = 1
 class Lattice2D:
     def __init__(self):
         self.spins_amount = 10
-        self.iterations = 500 * (self.spins_amount ** 2)
+        self.iterations = 50 * (self.spins_amount ** 2)
         self.spins = np.zeros((self.spins_amount, self.spins_amount), dtype=int)
         self.interaction_energy = 1  # J
         self.mu = 0.5
@@ -98,7 +98,100 @@ class Lattice2D:
         nominator = np.var(energy[anneal:] ** 2) + 4 * np.var(energy[anneal:]) * np.var(energy[anneal:])
         return (np.sqrt(nominator)) / ((self.spins_amount * self.temperature) ** 2)
 
-    def run(self):
+    def wolff_algorithm(self):
+        indexes = np.random.randint(low=0, high=self.spins_amount - 1, size=(2, 1))
+        i = indexes[0, 0]
+        j = indexes[1, 0]
+        root = (i, j)
+        self.build_cluster(root, self.spins[root])
+
+    def build_cluster(self, root, spin_value):
+
+        self.spins[root] *= -1
+        boundary_cond = self.spins_amount
+        neighbors = [(0, 0), (0, 0), (0, 0), (0, 0)]
+        (i, j) = root
+
+        if i == boundary_cond - 1:
+            neighbors[0] = (0, j)
+        else:
+            neighbors[0] = (i + 1, j)
+        if i == 0:
+            neighbors[1] = (boundary_cond - 1, j)
+        else:
+            neighbors[1] = (i - 1, j)
+        if j == boundary_cond - 1:
+            neighbors[2] = (i, 0)
+        else:
+            neighbors[2] = (i, j + 1)
+        if j == 0:
+            neighbors[3] = (i, boundary_cond - 1)
+        else:
+            neighbors[3] = (i, j - 1)
+
+        for next_site in neighbors:
+            if self.spins[next_site] == spin_value:
+                if np.random.random() < 1 - np.exp(-2.0/self.temperature):
+                    self.build_cluster(next_site, spin_value)
+
+    def run_wolff(self):
+        temperatures = np.linspace(self.temperature, 5, 50)
+        anneal = 9 * self.iterations // 10
+        energies = []
+        heat_capacities = []
+        magnetizations = []
+        energies_error = []
+        capacities_error = []
+        magnetizations_error = []
+        for index, each in enumerate(temperatures):
+            self.temperature = each
+            self.initiate_start_configuration()
+            tmp_energy = []
+            for each in range(self.iterations):
+                self.wolff_algorithm()
+                tmp_energy.append(self.calculate_internal_energy())
+            energy = np.array(tmp_energy)
+            mean_energy = np.mean(energy[anneal:])
+            mean_heat_capacity = (np.mean(energy[anneal:] ** 2) - mean_energy ** 2) / (
+                    (self.spins_amount * self.temperature) ** 2)
+            mean_magnetization = np.mean(self.spins)
+            logging.info(f'Temperature: {self.temperature} \n'
+                         f'Mean energy: {mean_energy}; \n'
+                         f'Mean heat capacity: {mean_heat_capacity} \n'
+                         f'Mean magnetization: {mean_magnetization} \n')
+            energies.append(mean_energy / (self.spins_amount ** 2))
+            heat_capacities.append(mean_heat_capacity)
+            magnetizations.append(mean_magnetization / (self.spins_amount ** 2))
+            energies_error.append(np.std(energy[anneal:]) / (self.spins_amount ** 2))
+            tmp_capacity_error = self.calculate_capacity_error(energy, anneal)
+            capacities_error.append(tmp_capacity_error)
+            magnetizations_error.append(np.std(self.spins) / (self.spins_amount ** 2))
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.errorbar(temperatures, energies, yerr=energies_error, fmt='o-', ecolor='green')
+        ax.set_xlabel(r'Temperature')
+        ax.set_ylabel(r'Energy/Spins')
+        ax.grid()
+        ax.set_title(r"Energies comparison")
+        plt.show()
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.errorbar(temperatures, heat_capacities, yerr=capacities_error, fmt='o-', ecolor='green')
+        ax.set_xlabel(r'Temperature')
+        ax.set_ylabel(r'Heat capacity')
+        ax.grid()
+        ax.set_title(r"Heat capacity changing due temperature")
+        plt.show()
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.errorbar(temperatures, np.power(magnetizations, 2), yerr=magnetizations_error, fmt='o-', ecolor='green')
+        ax.set_xlabel(r'Temperature')
+        ax.set_ylabel(r'Squared magnetization')
+        ax.grid()
+        ax.set_title(r"Squared magnetization due temperature")
+        plt.show()
+
+    def run_metropolis(self):
         temperatures = np.linspace(self.temperature, 5, 50)
         anneal = 9 * self.iterations // 10
         energies = []
@@ -113,7 +206,7 @@ class Lattice2D:
             energy = self.metropolis()
             mean_energy = np.mean(energy[anneal:])
             mean_heat_capacity = (np.mean(energy[anneal:] ** 2) - mean_energy ** 2) / (
-                (self.spins_amount * self.temperature) ** 2)
+                    (self.spins_amount * self.temperature) ** 2)
             mean_magnetization = np.mean(self.spins)
             logging.info(f'Temperature: {self.temperature} \n'
                          f'Mean energy: {mean_energy}; \n'
@@ -121,11 +214,11 @@ class Lattice2D:
                          f'Mean magnetization: {mean_magnetization} \n')
             energies.append(mean_energy / (self.spins_amount ** 2))
             heat_capacities.append(mean_heat_capacity)
-            magnetizations.append(mean_magnetization/ (self.spins_amount ** 2))
+            magnetizations.append(mean_magnetization / (self.spins_amount ** 2))
             energies_error.append(np.std(energy[anneal:]) / (self.spins_amount ** 2))
             tmp_capacity_error = self.calculate_capacity_error(energy, anneal)
             capacities_error.append(tmp_capacity_error)
-            magnetizations_error.append(np.std(self.spins)/(self.spins_amount ** 2))
+            magnetizations_error.append(np.std(self.spins) / (self.spins_amount ** 2))
 
         fig, ax = plt.subplots(figsize=(10, 6))
         ax.errorbar(temperatures, energies, yerr=energies_error, fmt='o-', ecolor='green')
@@ -152,7 +245,6 @@ class Lattice2D:
         plt.show()
 
 
-
 if __name__ == '__main__':
     lattice = Lattice2D()
-    lattice.run()
+    lattice.run_wolff()
